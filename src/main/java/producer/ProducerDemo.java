406edc379ad14bf768b47514a3665175ce85c4fd
Madan Nellore <nellorish@Madans-Mac-Pro.hsd1.de.comcast.net>
2020-09-08 11:56:37 -0400

    Initial Code Check inn

---
+++ src/main/java/producer/ProducerDemo.java
@@ -0,0 +1,25 @@
+package producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+
+    public static void main(String[] args) {
+
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        KafkaProducer<String,String> kafka = new KafkaProducer<String, String>(props);
+        ProducerRecord<String,String> record = new ProducerRecord<String, String>("first_topic", "Hello World");
+
+
+        System.out.println("Java is running as expected");
+    }
+}
